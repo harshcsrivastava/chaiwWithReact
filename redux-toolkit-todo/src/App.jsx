e4a284975daf386ba3b143cddf004bf7e2b1846d
Harsh Srivastava <harshcsrivastava@gmail.com>
2024-09-23 23:37:10 +0530

    Summary of React Redux

--- redux-toolkit-todo/src/App.jsx
+++ redux-toolkit-todo/src/App.jsx
@@ -20,6 +20,41 @@ function App() {
 
 export default App
 
-// STEP 1 : Configure Store
-// STEP 2 : Reducers (Slices in ReduxToolkit)
-            // todoSlice.js naming convention
+// REDUX - seperate lib
+// a predictable state container for JS apps
+
+// changes show be made using functions(reducers), Redux ke pahle FLUX tha(andrew), 
+// - store 
+// - reducers 
+// - useSelectors 
+// - useDispatch
+
+// Redux and React Redux alag hai
+// Redux => core Lib
+// React-Redux => uska implementation hai wiring karne ke liyr taki react aur redux me baatcheet ho
+
+// Step 1: Store banane se (Hamesha ek store banta) called Single Source of Truth 
+// // ek configureStore  chahiye jise hojayega 
+
+// Step 2: Reducer krna hai to, Store me jab value update hogi to har traf se nhi leta, data flow nhi hota
+// use batana padta reducer kaha hai
+
+// Step 3: Documentation me ise Slice bolte
+// Step 4: Need 3 chize => name, initialState(like use state), reducers ki list(obj)
+// usme jitne chahe key(functions) value pair daal skte {seperate file me bhi likh skte
+
+// Step 5: state, action milta, action.payload se value mili
+// Step 6: niche 10 reducer hai to 10 export kro, taki konsa component me konsa use hojaye
+// // and main-source bhi export
+
+// Step 7: AddTodo me value bhejni hai using const dispatch = useDispatch()
+// Step 8: Var me Lene ke liye `useSelector(state => state.todos )`  in callback
+
+// why in package.json private=true
+// because we don't want to publish our project on npm
+// means we don't want to share our project with others
+// so we make it private
+// what is the meaning of npm
+// npm is a package manager for the JavaScript programming language.
+// It is the default package manager for the JavaScript runtime environment Node.js.
+
